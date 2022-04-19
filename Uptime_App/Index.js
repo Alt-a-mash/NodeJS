c2f93ef3b2a324b7d6a790dff5652082c1a2a8f4
Altamash Ansari <aaltamash846@gmail.com>
2022-04-19 10:06:34 +0500

    Routed to ping

--- Uptime_App/Index.js
+++ Uptime_App/Index.js
@@ -116,10 +116,9 @@ var unifiedServer = function(req, res){
 // Define handlers
 var handlers = {};
 
-// Sample handler
-handlers.sample = function(data, callback){
-  // Callback a HTTP status code and a payload object
-  callback(406,{'name' : 'sample handler'});
+// Ping handler
+handlers.ping = function(data, callback){
+  callback(200);
 };
 
 // Not found handler
@@ -129,5 +128,5 @@ handlers.notFound = function(data, callback){
 
 // Define a request router
 var router = {
-  'sample' : handlers.sample
+  'ping' : handlers.ping
 };
